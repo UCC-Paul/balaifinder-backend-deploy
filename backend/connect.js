562eaa807f060bd9c14059e4425fe5faf74eff28
Paul <128304338+UCC-Paul@users.noreply.github.com>
2024-03-20 19:05:53 +0800

    UPDATE

--- backend/connect.js
+++ backend/connect.js
@@ -1,14 +1,13 @@
-
-
-
-
 // OLD CODE FOR LOCALHOST
 
 import mysql from "mysql";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "balaifinder_database",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DNAME,
 });
