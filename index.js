4f423969b52d6b97fd845065328232f08d4c6ae1
AluLicup <licup.alryan.bscs@gmail.com>
2024-04-27 14:29:25 +0800

    token2

--- index.js
+++ index.js
@@ -33,13 +33,24 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
+app.use((req, res, next) => {
+  // Add headers to allow cross-origin requests
+  res.setHeader("Access-Control-Allow-Origin", "https://balaifinder.vercel.app");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Add Authorization header if token is available
   const token = req.cookies.accessToken;
   if (token) {
     res.setHeader("Authorization", `Bearer ${token}`);
-  next();
   }
-  
+
+  // Proceed to the next middleware
+  next();
 });
 
 app.use("/api/auth", authRoutes);
