d5d684d660e3dcec87db4a3e7128f20c2a945e0b
AluLicup <licup.alryan.bscs@gmail.com>
2024-04-27 15:17:16 +0800

    userRotuer

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ import { getUserProfile, updateUserProfile } from '../controllers/user.js';
 const router = express.Router();
 
 // Define the route for fetching user profile
-router.get("/profile", getUserProfile);
+router.get("/:userId/profile", getUserProfile);
 
 // Route for updating user profile
-router.put('/profile', updateUserProfile);
+router.put('/:userId/profile', updateUserProfile);
 
 export default router;
