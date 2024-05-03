0378d4ba1bf9893203ffb29d85366b7ded969423
Paul <128304338+UCC-Paul@users.noreply.github.com>
2024-05-03 23:21:49 +0800

    Revert "ADDED NEW FRONTEND URL"
    This reverts commit a6b11bf84f13f9dec9691011471a6f0a3a263d6d.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   // Add headers to allow cross-origin requests
-  res.setHeader("Access-Control-Allow-Origin", "https://balaifinder.vercel.app, https://balaifinder-frontend-production.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://balaifinder.vercel.app");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Credentials", "true");
